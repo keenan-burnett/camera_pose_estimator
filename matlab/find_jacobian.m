e48976059b6aaa065de8b9ad22e3c9f3b22eb475
keenan-burnett <keenburn2004@gmail.com>
2017-10-27 23:28:51 -0400

    Just finished writing code for nonlinear least squares optimizer.

--- matlab/find_jacobian.m
+++ matlab/find_jacobian.m
@@ -13,12 +13,28 @@ function [J] = find_jacobian(K, Ec, Wpt)
 %
 %   Outputs:
 %   --------
-%    J  - 2x6 Jacobian matrix (columns are tx, ty, tz, r, p, q).
+%    J  - 2x6 Jacobian matrix (columns are tx, ty, tz, r, p, y).
 
 %--- FILL ME IN ---
 
-% Code goes here...
+R = Ec(1:3,1:3);
+t = Ec(1:3,4);
+y1 = Wpt - t;
+z = t(3);
 
+[dRdr, dRdp, dRdy] = dcm_jacob_rpy(R);  % Each is 3x3
+dRdr = K * dRdr' * y1;
+dRdp = K * dRdp' * y1;
+dRdy = K * dRdy' * y1;
+dfdtheta = [dRdr dRdp dRdy];
+
+dfdt = K * R.' * (-1);
+
+dfdP = [dfdt dfdtheta];     
+dzdP = dfdP(3,:);   % P = parameter vector
+
+f = K * R' * y1;     % f = distortion free, n = normalized, n = f / z, J = dndP
+J = (dfdP / z) - (f * dzdP) / z ^ 2; 
 %------------------
 
 end
--- matlab/pose_estimate_nlopt.m
+++ matlab/pose_estimate_nlopt.m
@@ -17,7 +17,60 @@ function [E] = pose_estimate_nlopt(Eg, Ipts, Wpts)
 
 %--- FILL ME IN ---
 
-% Code goes here...
+threshold = 10;
+E = Eg;
+R = E(1:3,1:3);
+t = E(1:3,4);
+q = rpy_from_dcm(R);
+P = [t ; q];
+K = [564.9 0 337.3; 0 564.3 226.5; 0 0 1];
+damp = eye(6) * 0.01;
+cost = 0;
+lastCost = 1e50;
+
+while (lastCost - cost) > threshold
+    % Get the rotation and translation from homogeneous pose
+    R = E(1:3,1:3);
+    t = E(1:3,4);
+    z = t(3);
+    
+    % Calculate A and B
+    A = 0;
+    b = 0;
+    for i = 1:48
+        xi = Wpts(:,i);
+        J_xi = find_jacobian(K, E, xi);
+        A = A + J_xi' * J_xi;
+        r_i = Ipts(:,i) - (K * R' * (xi - t) / z);
+        b = b + J_xi' * r_i;
+    end
+    
+    % Update P
+    deltaP = b / (A + damp);
+    P = P + deltaP;
+    
+    lastCost = cost;
+    cost = 0;
+    % Calculate cost
+    for i = 1:48
+        xi = Wpts(:,i);
+        J_xi = find_jacobian(K, E, xi);
+        r_i = Ipts(:,i) - (K * R' * (xi - t) / z);
+        cost = cost + norm(J_xi * deltaP - r_i) ^ 2;
+    end
+        
+    % Update pose matrix
+    E(1:3,1:3) = dcm_from_rpy(P(4:6));
+    E(1:3,4) = P(1:3);
+    
+end
+    
+    
+        
+        
+        
+    
+end
 
 %------------------
 
